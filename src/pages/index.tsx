cb19b3c5f01c53bbb2e3a6684526f316bc68ef09
Samus <26967164+SamusElderg@users.noreply.github.com>
2022-12-25 11:01:13 +0800

    change swap page structure
    - changed tiled structure to connected with a divider (divider changes x/y based on screen width)
    - removed a missed setInput(") from previous commit (asset2 icon onClick)
    - converted 'Connect Wallet' placeholder into a functional button to launch the wallet selection modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,7 @@ import { SwapSidePanel } from "../components/swap/swapSidePanel";
 
 import type { AssetProps } from "../components/assetSelect";
 import { gasDefault } from "../utils/const";
+import WalletModal from "../components/wallet/walletModal";
 export type AssetIdProps = 1 | 2;
 
 const button1 = { label: "GitHub?", link: "./" };
@@ -30,6 +31,7 @@ const button2 = { label: "Docs?", link: "./" };
 const Swap: NextPage = () => {
   const { address } = useAccount();
 
+  const [walletOpen, setWalletOpen] = useState(false);
   const [assetSelectOpen, setassetSelectOpen] = useState<boolean>(false);
   const [assetId, setassetId] = useState<AssetIdProps>(1);
   const [selectedAsset1, setSelectedAsset1] = useState<AssetProps>(bnbAsset);
@@ -46,6 +48,10 @@ const Swap: NextPage = () => {
     address: address,
     token: selectedAsset2.address,
   });
+
+  const toggleWallet = () => {
+    setWalletOpen(!walletOpen);
+  };
 
   const toggleAssetSelectOpen = (assetId: AssetIdProps) => {
     setassetId(assetId);
@@ -112,6 +118,7 @@ const Swap: NextPage = () => {
 
   return (
     <>
+      <WalletModal isOpen={walletOpen} setOpen={setWalletOpen} />
       <AssetSelect
         selectedAsset={assetId === 1 ? selectedAsset1 : selectedAsset2}
         setSelectedAsset={assetId === 1 ? setSelectedAsset1 : setSelectedAsset2}
@@ -120,163 +127,171 @@ const Swap: NextPage = () => {
       />
       <PageHeading title="Swap" button1={button1} button2={button2} />
       <PageWrap>
-        <ul className="grid grid-cols-1 gap-6 md:grid-cols-2">
-          <li id="first" className="col-span-1 divide-y divide-gray-200">
-            <div className="mx-auto h-full overflow-hidden rounded-lg bg-white shadow">
-              <div id="assetSection1" className="grid grid-cols-2 p-3">
-                <div className="w-max">Sell:</div>
-                <div className="w-max justify-self-end">
-                  {address ? (
-                    <>
-                      Balance:{" "}
-                      {assetBalance1.data
-                        ? formatFromWei(assetBalance1.data.value.toString())
-                        : "0.00"}
-                      <span
-                        className="ml-1 inline-flex items-center rounded bg-blue-100 px-2 py-0.5 text-xs font-medium text-blue-800"
-                        role="button"
-                        onClick={() =>
-                          assetBalance1.data &&
-                          setInput(
-                            convertFromWei(assetBalance1.data.value.toString())
-                          )
-                        }
-                      >
-                        MAX
-                      </span>
-                    </>
-                  ) : (
-                    "Connect Wallet"
-                  )}
-                </div>
-                <div
-                  onClick={() => toggleAssetSelectOpen(1)}
-                  role="button"
-                  className="w-max py-2"
-                >
-                  <span>
-                    <img
-                      src={selectedAsset1.logo}
-                      alt=""
-                      className="inline h-6 w-6 flex-none rounded-full"
-                    />
-                    <div className="ml-2 inline">{selectedAsset1.ticker}</div>
+        <ul className="grid grid-cols-1 divide-y divide-gray-200 rounded-lg bg-white shadow md:grid-cols-2 md:divide-y-0 md:divide-x">
+          <div className="">
+            <div id="assetSection1" className="grid grid-cols-2 p-3">
+              <div className="w-max">Sell:</div>
+              <div className="w-max justify-self-end">
+                {address ? (
+                  <>
+                    Balance:{" "}
+                    {assetBalance1.data
+                      ? formatFromWei(assetBalance1.data.value.toString())
+                      : "0.00"}
+                    <span
+                      className="ml-1 inline-flex items-center rounded bg-blue-100 px-2 py-0.5 text-xs font-medium text-blue-800"
+                      role="button"
+                      onClick={() =>
+                        assetBalance1.data &&
+                        setInput(
+                          convertFromWei(assetBalance1.data.value.toString())
+                        )
+                      }
+                    >
+                      MAX
+                    </span>
+                  </>
+                ) : (
+                  <span
+                    className="ml-1 inline-flex items-center rounded bg-blue-100 px-2 py-0.5 text-xs font-medium text-blue-800"
+                    role="button"
+                    onClick={() => toggleWallet()}
+                  >
+                    Connect Wallet
                   </span>
-                </div>
-                <div className="w-max justify-self-end py-2">
-                  <input
-                    placeholder="Buy Units"
-                    className="text-right"
-                    id="inputUnits1"
-                    onChange={(e) => setInputAmount(e.target.value)}
+                )}
+              </div>
+              <div
+                onClick={() => toggleAssetSelectOpen(1)}
+                role="button"
+                className="w-max py-2"
+              >
+                <span>
+                  <img
+                    src={selectedAsset1.logo}
+                    alt=""
+                    className="inline h-6 w-6 flex-none rounded-full"
                   />
-                  <XCircleIcon
-                    className="ml-1 mb-1 inline h-5 w-5 text-gray-700"
+                  <div className="ml-2 inline">{selectedAsset1.ticker}</div>
+                </span>
+              </div>
+              <div className="w-max justify-self-end py-2">
+                <input
+                  placeholder="Buy Units"
+                  className="text-right"
+                  id="inputUnits1"
+                  onChange={(e) => setInputAmount(e.target.value)}
+                />
+                <XCircleIcon
+                  className="ml-1 mb-1 inline h-5 w-5 text-gray-700"
+                  aria-hidden="true"
+                  role="button"
+                  onClick={() => setInput("")}
+                />
+              </div>
+              <div className="w-max">
+                {shortenString(selectedAsset1.address)}
+                <span>
+                  <DocumentDuplicateIcon
+                    className="ml-1 inline h-4 w-4 text-gray-700"
                     aria-hidden="true"
                     role="button"
-                    onClick={() => setInput("")}
+                    // onClick={() => TODO: Copy the address to clipboard }
                   />
-                </div>
-                <div className="w-max">
-                  {shortenString(selectedAsset1.address)}
-                  <span>
-                    <DocumentDuplicateIcon
-                      className="ml-1 inline h-4 w-4 text-gray-700"
-                      aria-hidden="true"
-                      role="button"
-                      // onClick={() => TODO: Copy the address to clipboard }
-                    />
-                    <ArrowTopRightOnSquareIcon
-                      className="ml-1 inline h-4 w-4 text-gray-700"
-                      aria-hidden="true"
-                      role="button"
-                      // onClick={() => TODO: Open wallet in explorer }
-                    />
-                  </span>
-                </div>
-                <div className="w-max justify-self-end">Rate: ??</div>
-              </div>
-              <div id="assetSection2" className="grid grid-cols-2 p-3">
-                <div className="w-max">Buy:</div>
-                <div className="w-max justify-self-end">
-                  {address ? (
-                    <>
-                      Balance:{" "}
-                      {assetBalance2.data
-                        ? formatFromWei(assetBalance2.data.value.toString())
-                        : "0.00"}
-                    </>
-                  ) : (
-                    "Connect Wallet"
-                  )}
-                </div>
-                <div
-                  onClick={() => toggleAssetSelectOpen(2)}
-                  role="button"
-                  className="w-max py-2"
-                >
-                  <span>
-                    <img
-                      src={selectedAsset2.logo}
-                      alt=""
-                      className="inline h-6 w-6 flex-none rounded-full"
-                      role="button"
-                      onClick={() => setInput("")}
-                    />
-                    <div className="ml-2 inline">{selectedAsset2.ticker}</div>
-                  </span>
-                </div>
-                <div className="w-max justify-self-end py-2">
-                  <input
-                    placeholder="Sell Units"
-                    className="text-right"
-                    id="inputUnits2"
-                    value={outputAmount}
-                    disabled
-                  />
-                  <ArrowPathIcon
-                    className={classNames(
-                      "ml-1 inline h-5 w-5 text-gray-700",
-                      isLoading ? "animate-spin" : ""
-                    )}
+                  <ArrowTopRightOnSquareIcon
+                    className="ml-1 inline h-4 w-4 text-gray-700"
                     aria-hidden="true"
                     role="button"
-                    onClick={() => onInputChange(inputAmount)}
+                    // onClick={() => TODO: Open wallet in explorer }
                   />
-                </div>
-                <div className="w-max">
-                  {shortenString(selectedAsset2.address)}
-                  <span>
-                    <DocumentDuplicateIcon
-                      className="ml-1 inline h-4 w-4 text-gray-700"
-                      aria-hidden="true"
-                      role="button"
-                      // onClick={() => TODO: Copy the address to clipboard }
-                    />
-                    <ArrowTopRightOnSquareIcon
-                      className="ml-1 inline h-4 w-4 text-gray-700"
-                      aria-hidden="true"
-                      role="button"
-                      // onClick={() => TODO: Open wallet in explorer }
-                    />
+                </span>
+              </div>
+              <div className="w-max justify-self-end">Rate: ??</div>
+            </div>
+            <div id="assetSection2" className="grid grid-cols-2 p-3">
+              <div className="w-max">Buy:</div>
+              <div className="w-max justify-self-end">
+                {address ? (
+                  <>
+                    Balance:{" "}
+                    {assetBalance2.data
+                      ? formatFromWei(assetBalance2.data.value.toString())
+                      : "0.00"}
+                  </>
+                ) : (
+                  <span
+                    className="ml-1 inline-flex items-center rounded bg-blue-100 px-2 py-0.5 text-xs font-medium text-blue-800"
+                    role="button"
+                    onClick={() => toggleWallet()}
+                  >
+                    Connect Wallet
                   </span>
-                </div>
-                <div className="w-max justify-self-end">Rate: ??</div>
+                )}
               </div>
-              <div className="w-full border-t border-gray-300" />
-              <div id="swapInfoSection" className="p-3">
-                <span>Rate</span>
-                <span className="float-right">Fees</span>
-                <div>PCS Comparison Rate</div>
-                <div>Simple Route Info</div>
+              <div
+                onClick={() => toggleAssetSelectOpen(2)}
+                role="button"
+                className="w-max py-2"
+              >
+                <span>
+                  <img
+                    src={selectedAsset2.logo}
+                    alt=""
+                    className="inline h-6 w-6 flex-none rounded-full"
+                    role="button"
+                  />
+                  <div className="ml-2 inline">{selectedAsset2.ticker}</div>
+                </span>
               </div>
+              <div className="w-max justify-self-end py-2">
+                <input
+                  placeholder="Sell Units"
+                  className="text-right"
+                  id="inputUnits2"
+                  value={outputAmount}
+                  disabled
+                />
+                <ArrowPathIcon
+                  className={classNames(
+                    "ml-1 inline h-5 w-5 text-gray-700",
+                    isLoading ? "animate-spin" : ""
+                  )}
+                  aria-hidden="true"
+                  role="button"
+                  onClick={() => onInputChange(inputAmount)}
+                />
+              </div>
+              <div className="w-max">
+                {shortenString(selectedAsset2.address)}
+                <span>
+                  <DocumentDuplicateIcon
+                    className="ml-1 inline h-4 w-4 text-gray-700"
+                    aria-hidden="true"
+                    role="button"
+                    // onClick={() => TODO: Copy the address to clipboard }
+                  />
+                  <ArrowTopRightOnSquareIcon
+                    className="ml-1 inline h-4 w-4 text-gray-700"
+                    aria-hidden="true"
+                    role="button"
+                    // onClick={() => TODO: Open wallet in explorer }
+                  />
+                </span>
+              </div>
+              <div className="w-max justify-self-end">Rate: ??</div>
             </div>
-          </li>
-          <li id="second" className="col-span-1 divide-y divide-gray-200">
-            <div className="mx-auto h-full overflow-hidden rounded-lg bg-white shadow">
-              <SwapSidePanel />
+            <div className="w-full border-t border-gray-300" />
+            <div id="swapInfoSection" className="p-3">
+              <span>Rate</span>
+              <span className="float-right">Fees</span>
+              <div>PCS Comparison Rate</div>
+              <div>Simple Route Info</div>
             </div>
-          </li>
+          </div>
+
+          <div className="">
+            <SwapSidePanel />
+          </div>
         </ul>
       </PageWrap>
     </>
